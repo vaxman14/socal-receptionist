7352450de4982b02d32f4f53d43257f83a93a82e
Roman Vaxman <vaxman14@gmail.com>
2026-06-08 19:49:01 -0700

    Add /roadmap route to server

--- server.js
+++ server.js
@@ -169,6 +169,8 @@ if (process.env.COMING_SOON === 'true') {
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/roadmap', (req, res) => res.sendFile(path.join(__dirname, 'public', 'roadmap.html')));
+
 // Inject analytics tags into the landing page HTML at request time so
 // GTM_ID / GA_ID / FB_PIXEL_ID env vars take effect without a redeploy.
 app.use((req, res, next) => {
